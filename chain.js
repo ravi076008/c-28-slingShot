9f2bb6d04005785dacbb933e013c95910ce3d5da
ravi076008 <ksBhat69@yahoo.com>
2021-01-04 21:16:45 +0530

    chain

---
+++ chain.js
@@ -0,0 +1,31 @@
+class Chain{
+    constructor(Body1,Body2){
+        var options= {
+          bodyA:Body1 ,
+          bodyB:Body2,
+          stiffness:0.5,
+          length:10
+
+        }
+
+
+
+        
+this.chain=Matter.Constraint.create(options);
+World.add(world,this.chain)
+
+    }
+display(){
+    var pos=this.chain.bodyA.position
+    var pos2=this.chain.bodyB.position
+line(pos.x,pos.y,pos2.x,pos2.y);
+
+
+
+
+
+}
+
+}
+
+
--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain1 = new Chain(bird.body,log5.body)
 
 }
 
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain1.display();
 }
